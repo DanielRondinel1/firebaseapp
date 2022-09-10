120a736f57a0f874d5a92b03a37ed6acdd644bfd
DanielrDGT <daniel.rondinel@dgtalliance.com>
2022-09-10 17:06:02 -0500

    prueba1

---
+++ app/src/main/java/com/drondinel/firebaseapp/kokoko.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel on 10/09/2022.
+//
+
+#include "kokoko.h"
---
+++ app/src/main/java/com/drondinel/firebaseapp/kokoko.h
@@ -0,0 +1,16 @@
+//
+// Created by Daniel on 10/09/2022.
+//
+
+#ifndef FIREBASEAPP_KOKOKO_H
+#define FIREBASEAPP_KOKOKO_H
+
+
+
+class kokoko {
+
+};
+
+
+
+#endif //FIREBASEAPP_KOKOKO_H
